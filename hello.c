dcf35df7ff2c82ff952eedfe4fd563dd3882bbe1
Triquetrus <salmanbagwan6002@gmail.com>
2026-07-04 23:43:16 +0530

    Starting DSA

--- hello.c
+++
@@ -1,9 +0,0 @@
-//Testing C and git
-#include <stdio.h>
-
-int main() {
-    printf("Hello, DSA is ready!\n");
-    printf("Learning to work with Git");
-    printf("Making first changes to code to see the results");
-    return 0;
-}
