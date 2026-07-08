840e49d28afc474202122dff9470a64460c5c1ce
Triquetrus <salmanbagwan6002@gmail.com>
2026-07-08 23:14:50 +0530

    Hashmap traversal

---
+++ 01_Java_Fundamentals/HashMap/Hashmap.java
@@ -0,0 +1,28 @@
+/*Given an array:
+
+int[] numbers = {7, 2, 7, 5, 2, 7, 9};
+
+Write a program that counts how many times each number appears using a HashMap and KeySet() using for each loop. */
+
+import java.util.*;
+public class Hashmap-04 {
+     public static void main(String args[])
+     {
+        int[] numbers = {7, 2, 7, 5, 2, 7, 9};
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int num : numbers)
+        {
+            if(map.containsKey(num))
+            {
+                map.put(num,map.get(num)+1);
+            }else{
+                map.put(num,1);
+            }
+        }
+        for(int num : map.KeySet()
+        {
+            System.out.println(num+"->"+ map.get(num));
+        })
+     }
+}
